9e208a862838d5b5d2f1cec03cd6eb82e6721c8f
Nabila Ayaba <nabilaayaba@gmail.com>
2025-06-29 11:09:01 +0300

    Create play, render, and getComputerChoice functions

--- js/app.js
+++ js/app.js
@@ -16,7 +16,32 @@ const scissorsBtnEl = document.querySelector('#scissors')
 
 
 /*-------- Functions -----------------------*/
+play()
 
+
+function getComputerChoice() {
+    // generate a random number 0-2
+    const randomIndex = Math.floor(Math.random() * choice.length)
+    // select the item from the array
+    return choice[randomIndex]
+}
+
+// initialize game state
+function play() {
+    console.log('computerChoice', computerChoice)
+    computerChoice = getComputerChoice()
+    console.log('computerChoice', computerChoice)
+
+    // after updating state, render to html
+    render()
+}
+
+
+
+// updates our UI/html directly
+function render() {
+
+}
 
 
 /*--------- Event Listeners ----------------*/
