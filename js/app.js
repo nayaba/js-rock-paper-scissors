d5d4428f30de3987de7ddd6bf964722a962e9422
Nabila Ayaba <nabilaayaba@gmail.com>
2025-06-29 10:44:28 +0300

    Cache buttons and create state variables

---
+++ js/app.js
@@ -0,0 +1,22 @@
+/*------------ Constants ------------------*/
+const choice = ['rock', 'paper', 'scissors']
+
+
+/*------------- Variables -----------------*/
+let msg
+let playerChoice
+let computerChoice
+
+
+/*------ Cached Element References ---------*/
+const rockBtnEl = document.querySelector('#rock')
+const paperBtnEl = document.querySelector('#paper')
+const scissorsBtnEl = document.querySelector('#scissors')
+
+
+
+/*-------- Functions -----------------------*/
+
+
+
+/*--------- Event Listeners ----------------*/
